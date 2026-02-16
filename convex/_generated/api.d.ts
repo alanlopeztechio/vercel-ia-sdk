456271ca086d49bc773573ddb4cba3ace7c1953e
alanlopez0208@ <alan666_alan@hotmail.com>
2026-02-16 13:58:41 -0600

    feat: Add users module to fullApi for enhanced functionality

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as padres from "../padres.js";
+import type * as users from "../users.js";
 
 import type {
   ApiFromModules,
@@ -18,6 +19,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   padres: typeof padres;
+  users: typeof users;
 }>;
 
 /**
