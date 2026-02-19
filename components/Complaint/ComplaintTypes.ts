9c87b81b42f505ad850b0300928622e037e8da92
alanlopez0208@ <alan666_alan@hotmail.com>
2026-02-19 14:09:50 -0600

    feat: remove complaint components and related files

--- components/Complaint/ComplaintTypes.ts
+++
@@ -1,9 +0,0 @@
-import { Id } from '../../convex/_generated/dataModel';
-
-export interface ComplaintDTO {
-  id?: Id<'complaints'>;
-  title: string;
-  comment: string;
-  idPurchase: Id<'purchases'>;
-  idUser: Id<'users'>;
-}
